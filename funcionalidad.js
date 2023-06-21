1fbc9b6df7770ebec2159ebee7014ef0cfb65a41
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-20 21:35:42 -0300

    Se configura movimiento de teclas

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,12 +1,80 @@
+
+const MARGEN_TABLERO = 10 ; 
+
+let regulador_velocidad_teclas = 0 ;
+
 function setup (){
     createCanvas(900,600) ;
     tablero = new Tablero ()
+    tetrimino = new Tetrimino()
+    resizeCanvas(
+        tablero.ancho + 2*MARGEN_TABLERO,
+        tablero.alto + 2*MARGEN_TABLERO)
 }
+
+function keyEventsTetris(){
+    if( millis() - regulador_velocidad_teclas < 200){
+        return
+    }
+    regulador_velocidad_teclas = millis()
+
+    if(keyIsDown(RIGHT_ARROW)){
+        tetrimino.moverDerecha()
+    }
+    if(keyIsDown(LEFT_ARROW)){
+        tetrimino.moverIzquierda()
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        tetrimino.moverAbajo()
+    }
+    if(keyIsDown(UP_ARROW)){
+        tetrimino.moverArriba()
+    }
+}
+
 
 function draw (){
     background("lightgray") ;
     tablero.dibujar()
+    tetrimino.dibujar()
+    keyEventsTetris()
 }
+
+
+class Tetrimino{
+
+    constructor(){
+        this.position = createVector( 0 , 0)
+    }
+
+    moverDerecha(){
+        this.position.x++
+    }
+
+    moverIzquierda(){
+        this.position.x--
+    }
+    moverArriba(){
+        this.position.y--
+    }
+    moverAbajo(){
+        this.position.y++
+    }
+
+
+    dibujar(){
+        push()
+        fill("red")
+        let coord = tablero.coordenada( this.position.x , this.position.y)
+        rect( coord.x , coord.y , tablero.lado_celda)
+        pop()
+    }
+}
+
+
+
+
+
 
 class Tablero {
     constructor(){
@@ -15,7 +83,7 @@ class Tablero {
         this.lado_celda=25
         this.ancho=  this.columnas * this.lado_celda
         this.alto = this.filas * this.lado_celda
-        this.position = createVector(0 , 0)
+        this.position = createVector( MARGEN_TABLERO , MARGEN_TABLERO ) //Para centrar el Canvas (width - this.ancho) / 2 ,  (height- this.alto) / 2
     }
 
     coordenada( x , y ){
@@ -23,11 +91,18 @@ class Tablero {
     }
 
     dibujar(){
+       push()
+       noStroke()
         for(let columna = 0 ; columna < this.columnas ; columna++){
             for(let fila = 0 ; fila < this.filas ; fila++){
+                if( (columna+fila)%2 ===0 ){
+                    fill("black")
+                }else{
+                    fill("#003")
+                }
                 let c = this.coordenada( columna, fila)  // C = Columna
                 rect( c.x , c.y , this.lado_celda)
-      }
+         }}
+       pop()
     }
   }
-}
